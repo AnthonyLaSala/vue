8e7e07a1ea89c8cab9bacbd9eaeda9f165a965c8
anthony.lasala <anthony.lasala@afl.com.au>
2018-01-09 11:39:01 +1100

    Filters and Mixins

---
+++ filters-mixins/src/List.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row">
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits"> {{ fruit }}</li>
+                </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { fruitMixin } from './fruitMixin';
+
+    export default {
+        mixins: [fruitMixin],
+        created(){
+            console.log('Inside list created hook');
+        }
+    }
+</script>
+
+<style>
+
+</style>

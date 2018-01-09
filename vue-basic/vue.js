7b61c3eadb32302b084b84cec9bbc5b7b522d943
anthony.lasala <anthony.lasala@afl.com.au>
2018-01-09 11:31:37 +1100

    Basics

---
+++ vue-basic/vue.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+})
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})

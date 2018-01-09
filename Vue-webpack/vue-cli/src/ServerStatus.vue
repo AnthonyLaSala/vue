e87ee99c6fcc74cb2acf5acd1699ad6bb25e9f60
anthony.lasala <anthony.lasala@afl.com.au>
2018-01-09 11:34:08 +1100

    Vue Webpack and CLI

---
+++ Vue-webpack/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p> Server  Status: {{ status }}</p>
+    <button @click="changeStatus">Change Status</button>
+    <hr>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return{
+        status: 'Critical'
+      }
+    },
+    methods: {
+      changeStatus(){
+        this.status = 'Normal';
+      }
+    }
+  }
+
+</script>
+
+
+<style scoped>
+/* scoped only applies to current component */
+
+  div{
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+</style>
